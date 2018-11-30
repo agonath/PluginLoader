ea6d61f605415fa3e3a2797f457b1a71036b588e
agonath <agonath@outlook.com>
2018-11-30 15:07:20 +0100

    Setup Datei hinzugefügt.

---
+++ bin/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup( 
+     name = "PluginLoader",
+     version = "0.1",
+     author = "Agonath",
+     author_email = "Agonath@outlook.com",
+     description = "Loads a module or a class object by the given path. Can be used as plugin loading system.",
+     license = "Do what yout want at your own risk. Its free.",
+     url = "https://github.com/agonath/PluginLoader",
+     download_url = "https://github.com/agonath/PluginLoader",
+     packages = ["PluginLoader"]
+     )
